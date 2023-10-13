7d71d2366a27c7322f2da44a2d09f45ad16ca54f
Felipe Losano <felipelosano@outlook.com>
2023-10-13 12:21:55 -0300

    Mudaças no layout e links

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,33 @@
 export const dataFake = [
   {
-    "id":"1",
-    "title": "NOVO HOMEM DE FERRO EM 3D",
-    "description": "marvel anuncia um novo filme do homem de ferro, confira",
-    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+    id: '1',
+    title: 'Atualização de Valorant',
+    description:
+      'Nova atualização de Valorant traz novos personagens e novos mapas',
+    photoCover:
+      'https://cdn1.epicgames.com/offer/cbd5b3d310a54b12bf3fe8c41994174f/EGS_VALORANT_RiotGames_S1_2560x1440-b88adde6a57e40aa85818820aa87a6cd',
   },
   {
-    "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
-    "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
-  }
-]
+    id: '2',
+    title: 'Nova atualização de League of Legends',
+    description:
+      'Nova atualização de League of Legends traz novos personagens e novos itens',
+    photoCover:
+      'https://cdn1.epicgames.com/offer/24b9b5e323bc40eea252a10cdd3b2f10/EGS_LeagueofLegends_RiotGames_S1_2560x1440-2935d0a3e332decb8e727fe56789b6ab',
+  },
+  {
+    id: '3',
+    title: 'Nova atualização de Green Hell',
+    description:
+      'Nova atualização de Green Hell traz novos itens e novos inimigos',
+    photoCover:
+      'https://cdn.akamai.steamstatic.com/steam/apps/815370/header_alt_assets_5.jpg?t=1696344693',
+  },
+  {
+    id: '4',
+    title: 'Lançamento: Sons of the Forest',
+    description: 'Novo lançamento de Sons of the Forest',
+    photoCover:
+      'https://cdn.cloudflare.steamstatic.com/steam/apps/1326470/capsule_616x353.jpg?t=1697048131',
+  },
+];
